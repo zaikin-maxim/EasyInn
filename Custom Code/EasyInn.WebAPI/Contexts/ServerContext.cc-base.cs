5a1268455ad5f7bb294110fc0e183a4f695c08cc
Incold0310 <130466@rambler.ru>
2020-01-16 15:48:06 +0300

    Add project

---
+++ Custom Code/EasyInn.WebAPI/Contexts/ServerContext.cc-base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BusinessFramework.WebAPI.Contracts.Files;
+using EasyInn.WebAPI.Contracts.Contexts;
+
+
+namespace EasyInn.WebAPI.Contexts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ServerContext : IServerContext
+    {    
+        /// <summary>
+        /// 
+        /// </summary>
+		public int CurrentUserId
+		{
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Custom Code/EasyInn.WebAPI/Contexts/ServerContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BusinessFramework.WebAPI.Contracts.Files;
+using EasyInn.WebAPI.Contracts;
+using EasyInn.WebAPI.Contracts.Contexts;
+using EasyInn.WebAPI.Contracts.Repositories;
+
+
+namespace EasyInn.WebAPI.Contexts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ServerContext : IServerContext
+    {
+        private readonly ISecurityService _securityService;
+        private readonly IUserExtensionRepository _userExtensionRepository;
+
+
+        public ServerContext(ISecurityService securityService,
+            IUserExtensionRepository userExtensionRepository
+        )
+        {
+            _securityService = securityService;
+            _userExtensionRepository = userExtensionRepository;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+		public int CurrentUserId
+		{
+            get { return _securityService.CurrentUser.Id; }
+        }
+    }
+}
